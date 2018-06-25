6cf6948306328e5ee93e2307a4ff06f4ece95c59
加文雄 <jiawenxiong@newbeescm.com>
2018-06-25 19:16:14 +0800

    nothing

--- git-server/src/main/java/com/architect/git/service/HelloGit.java
+++ git-server/src/main/java/com/architect/git/service/HelloGit.java
@@ -3,6 +3,5 @@ package com.architect.git.service;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("修复101bug");
     }
 }
