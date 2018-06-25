46c33982e8d8831371e65ac4716fcd3f33e11203
加文雄 <jiawenxiong@newbeescm.com>
2018-06-25 20:37:49 +0800

    nothing

--- git-server/src/main/java/com/architect/git/service/RebaseGit.java
+++ git-server/src/main/java/com/architect/git/service/RebaseGit.java
@@ -3,6 +3,5 @@ package com.architect.git.service;
 public class RebaseGit {
     public static void main(String[] args) {
         System.out.println("变基");
-        System.out.println("rebase");
     }
 }
