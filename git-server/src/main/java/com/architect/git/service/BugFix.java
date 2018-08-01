51880743a52bd669f6065071d525fbfffa523a66
加文雄 <jiawenxiong@newbeescm.com>
2018-08-01 13:44:47 +0800

    创建修复bug9527分支

---
+++ git-server/src/main/java/com/architect/git/service/BugFix.java
@@ -0,0 +1,11 @@
+package com.architect.git.service;
+
+/**
+ * @author wenxiong.jia
+ * @since 2018/8/1
+ */
+public class BugFix {
+    public static void main(String[] args) {
+        System.out.println("修复bug9527");
+    }
+}
