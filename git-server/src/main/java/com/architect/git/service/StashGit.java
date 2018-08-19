558fdfef0106cd8b1c5c1d892f9045fac6a1569d
加文雄 <jiawenxiong@newbeescm.com>
2018-08-19 19:41:42 +0800

    nothing

--- git-server/src/main/java/com/architect/git/service/StashGit.java
+++ git-server/src/main/java/com/architect/git/service/StashGit.java
@@ -3,5 +3,6 @@ package com.architect.git.service;
 public class StashGit {
     public static void main(String[] args) {
         System.out.println("修复101bug");
+        System.out.println("dfdfdfdf");
     }
 }
