0fd1edc2430240153112528882473772f9a9c08d
加文雄 <jiawenxiong@newbeescm.com>
2018-08-24 15:05:46 +0800

    nothing

--- git-server/src/main/java/com/architect/git/service/HelloGit.java
+++ git-server/src/main/java/com/architect/git/service/HelloGit.java
@@ -4,6 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("冲突测试-用户2");
-        System.out.println("接收我的");
+        System.out.println("接收我的dd");
     }
 }
